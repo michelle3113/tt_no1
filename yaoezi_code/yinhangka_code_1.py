025712e361a0853335b5c35e9fddaa6d7f1be34b
yifanfeng97 <evanfeng97@gmail.com>
2020-02-16 17:56:52 +0800

    yinhangka yaoezi code

--- yaoezi_code/yinhangka_code_1.py
+++ yaoezi_code/yinhangka_code_1.py
@@ -7,25 +7,25 @@ def revise_parse(s: str):
     return [25 - (ord(c.lower()) - ord('a')) for c in s]
 
 
-def cvt_by_mat_dot(source, key_mat):
+def cvt_by_mat_dot(code, key_mat):
     res = []
     idx = 0
-    while idx + 9 < len(source):
-        tmp_mat = np.mat(source[idx: idx + 9]).reshape(3, 3)
-        tar_mat = tmp_mat * key_mat
+    while idx + 9 < len(code):
+        tmp_mat = np.mat(code[idx: idx + 9]).reshape(3, 3)
+        tar_mat = key_mat * tmp_mat
         res.extend(tar_mat.reshape(-1).tolist()[0])
         idx += 9
-    res.extend(source[idx:])
+    res.extend(code[idx:])
     return res
 
 
 if __name__ == '__main__':
-    source_str = 'OFRNHVTNVWTZNCVCMBZZZ'
+    code = 'OFRNHVTNVWTZNCVCMBZZZ'
     key_mat = np.mat([
         [1, 2, 1],
         [3, 0, 2],
         [1, 1, 0],
     ])
-    target = revise_parse(source_str)
-    target = cvt_by_mat_dot(target, key_mat)
-    print(target)
+    code = revise_parse(code)
+    code = cvt_by_mat_dot(code, key_mat)
+    print(code)
