43169b61f9bcd77266d1dde5ea223e834da93dd0
yifanfeng97 <evanfeng97@gmail.com>
2020-02-16 22:16:31 +0800

    love you tiaotiao~

--- metrics.py
+++ metrics.py
@@ -1,1 +1,8 @@
 # love you tons! tiaotiao
+import numpy as np
+
+
+# metric for MAPE
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
