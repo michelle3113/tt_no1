2a0d6eaa00417b0b1383502bfed68388c67ce4ca
yifanfeng97 <evanfeng97@gmail.com>
2020-02-16 22:23:27 +0800

    love you tiaotiao~

--- metrics.py
+++ metrics.py
@@ -6,3 +6,10 @@ import numpy as np
 def mean_absolute_percentage_error(y_true, y_pred):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+
+def acc(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    N_tp = ((y_true >= 1) and (y_pred >= 1)).sum()
+    N_tn = ((y_true >= 1) and (y_pred == 0)).sum()
+    return (N_tp + N_tn) / y_true.size()
