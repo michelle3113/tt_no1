5f073a3be50eed8fa4aea164b98602089211035e
yifanfeng97 <evanfeng97@gmail.com>
2020-02-23 12:05:41 +0800

    add lung visualization!

---
+++ tmp/feiyan_vis.py
@@ -0,0 +1,33 @@
+from skimage import measure
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+
+def plot_3d(image, threshold=-300):
+    # Position the scan upright,
+    # so the head of the patient would be at the top facing the camera
+    p = image.transpose(2, 1, 0)
+
+    verts, faces = measure.marching_cubes_lewiner(p, threshold)
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Fancy indexing: `verts[faces]` to generate a collection of triangles
+    mesh = Poly3DCollection(verts[faces], alpha=0.70)
+    face_color = [0.45, 0.45, 0.75]
+    mesh.set_facecolor(face_color)
+    ax.add_collection3d(mesh)
+
+    ax.set_xlim(0, p.shape[0])
+    ax.set_ylim(0, p.shape[1])
+    ax.set_zlim(0, p.shape[2])
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    filename = 'C:\\Users\\yifan\\Desktop\\feiyan\\tmp\\1.npy'
+    volume = np.load(filename)
+    plot_3d(volume)
