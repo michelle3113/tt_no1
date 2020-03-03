170ae296f2d2d806ced3428964a6c67de3ab75bf
yifanfeng97 <evanfeng97@gmail.com>
2020-03-03 14:33:45 +0800

    add lung visualization!

--- tmp/feiyan_vis.py
+++ tmp/feiyan_vis.py
@@ -4,12 +4,31 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
+def HU2uint8(image, HU_min=-1200.0, HU_max=600.0, HU_nan=-2000.0):
+    """
+    Convert HU unit into uint8 values. First bound HU values by predfined min
+    and max, and then normalize
+    image: 3D numpy array of raw HU values from CT series in [z, y, x] order.
+    HU_min: float, min HU value.
+    HU_max: float, max HU value.
+    HU_nan: float, value for nan in the raw CT image.
+    """
+    image_new = np.array(image)
+    image_new[np.isnan(image_new)] = HU_nan
+
+    # normalize to [0, 1]
+    image_new = (image_new - HU_min) / (HU_max - HU_min)
+    image_new = np.clip(image_new, 0, 1)
+    image_new = (image_new * 255).astype('uint8')
+
+    return image_new
+
 def plot_3d(image, threshold=-300):
     # Position the scan upright,
     # so the head of the patient would be at the top facing the camera
     p = image.transpose(2, 1, 0)
 
-    verts, faces = measure.marching_cubes_lewiner(p, threshold)
+    verts, faces, norm, val = measure.marching_cubes_lewiner(p, threshold)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111, projection='3d')
@@ -30,4 +49,10 @@ def plot_3d(image, threshold=-300):
 if __name__ == '__main__':
     filename = 'C:\\Users\\yifan\\Desktop\\feiyan\\tmp\\1.npy'
     volume = np.load(filename)
-    plot_3d(volume)
+    plot_3d(volume, 0)
+
+    # volume_vis = HU2uint8(volume)
+    # plt.imshow(volume_vis[10], cmap=plt.cm.gray)
+    # plt.show()
+
+    pass
